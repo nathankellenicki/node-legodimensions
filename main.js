fd5bae55128b69ed7ffc6cc220fbadf643ebc50b
Nathan Kunicki <me@nathankunicki.com>
2016-06-18 00:42:25 +0100

    Testing color setting

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let toyPad = new ToyPad();
 
 toyPad.on("connect", () => {
     console.log("Toy Pad connected");
-    toyPad.setColor(ToyPad.Panel.CENTER, 0xffff44, 0.5);
 });
 
 toyPad.on("error", () => {
@@ -14,11 +13,17 @@ toyPad.on("error", () => {
 toyPad.on("add", (data) => {
     let name = ToyPad.minifigNameFromSignature(data.sig);
     console.log(`Minifig added to panel ${data.panel} (${name})`);
+    if (data.panel == ToyPad.Panel.CENTER) {
+        toyPad.setColor(data.panel, 0xffff44, 0.5);
+    }
 });
 
 toyPad.on("remove", (data) => {
     let name = ToyPad.minifigNameFromSignature(data.sig);
     console.log(`Minifig removed from panel ${data.panel} (${name})`);
+    if (data.panel == ToyPad.Panel.CENTER) {
+        toyPad.setColor(data.panel, 0, 0.9);
+    }
 });
 
 toyPad.connect();
